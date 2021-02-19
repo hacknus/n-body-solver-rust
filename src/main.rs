1455c9cb2de74e6a0cf388656435f4e77d2dcc67
hacknus <l_stoeckli@bluewin.ch>
2021-02-19 22:16:11 +0100

    implemented csv read in

--- src/main.rs
+++ src/main.rs
@@ -11,37 +11,13 @@ use crate::io::read_csv;
 
 fn main() {
     println!("Hello, Rust!");
-    read_csv();
-    let mut bodies: Vec<Body> = Vec::new();
+    let mut bodies: Vec<Body> = match read_csv("SolSystData.dat") {
+        Err(e) => panic!("Problem opening the file: {:?}", e),
+        Ok(b) => b,
+    };
     let steps: u32 = 100;
     let mut dt: f64;
     let mut t: f64 = 0.0;
-    bodies.push(Body {
-        m: 1.0,
-        x: -1.0,
-        y: 0.0,
-        z: 0.0,
-        vx: 0.0,
-        vy: 1.0,
-        vz: 0.0,
-        ax: 0.0,
-        ay: 0.0,
-        az: 0.0,
-        softening: 0.0,
-    });
-    bodies.push(Body {
-        m: 1.0,
-        x: 1.0,
-        y: 0.0,
-        z: 0.0,
-        vx: 0.0,
-        vy: -1.0,
-        vz: 0.0,
-        ax: 0.0,
-        ay: 0.0,
-        az: 0.0,
-        softening: 0.0,
-    });
 
     calc_direct_force(&mut bodies);
 
