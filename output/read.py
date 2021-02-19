d0e661769b4bc010b8188cbf7ba2cea00aefa0c3
hacknus <l_stoeckli@bluewin.ch>
2021-02-19 23:03:00 +0100

    added python file to read binary output files

---
+++ output/read.py
@@ -0,0 +1,29 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import mpl_toolkits.mplot3d.axes3d as p3
+import matplotlib.animation as animation
+import sys
+import os
+import pandas as pd
+
+
+def read_binary(path):
+    # path = "out_10.bin"
+    x = np.fromfile(path, dtype=np.float64)
+
+    x = x.reshape((8, 9))
+
+    d = {"x": x[:, 0],
+         "y": x[:, 1],
+         "z": x[:, 2],
+         "vx": x[:, 3],
+         "vy": x[:, 4],
+         "vz": x[:, 5],
+         }
+    df = pd.DataFrame(data=d)
+    print(df.head())
+    return df
+
+
+if __name__ == "__main__":
+    read_binary("out1.dat")
