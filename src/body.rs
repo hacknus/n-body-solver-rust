3f8689f2e90a1327167988b0e6e40eb1bcb833ad
hacknus <l_stoeckli@bluewin.ch>
2022-02-18 13:40:51 +0100

    introduced ID and empty body

--- src/body.rs
+++ src/body.rs
@@ -3,6 +3,7 @@ use std::fmt;
 
 #[derive(Debug, Clone, Copy)]
 pub struct Body {
+    pub id: u32,
     pub m: Real,
     pub x: Real,
     pub y: Real,
@@ -16,8 +17,23 @@ pub struct Body {
     pub softening: Real,
 }
 
+pub const EMPTY_BODY: Body = Body {
+    id: 0,
+    m: 0.0,
+    x: 0.0,
+    y: 0.0,
+    z: 0.0,
+    vx: 0.0,
+    vy: 0.0,
+    vz: 0.0,
+    ax: 0.0,
+    ay: 0.0,
+    az: 0.0,
+    softening: 0.001,
+};
+
 impl fmt::Display for Body {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "m = {}, pos = [{},{},{}], vel = [{},{},{}]", self.m, self.x, self.y, self.z, self.vx, self.vy, self.vz)
+        write!(f, "id = {}, m = {}, pos = [{},{},{}], vel = [{},{},{}]", self.id, self.m, self.x, self.y, self.z, self.vx, self.vy, self.vz)
     }
 }
