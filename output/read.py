1d80d544281ef763292dfb2276a9cb8a412d0b7a
hacknus <linus.stoeckli@unibe.ch>
2023-02-25 01:06:26 +0100

    python fix

--- output/read.py
+++ output/read.py
@@ -52,186 +52,186 @@ def read_binary(path):
     df = pd.DataFrame(data=d)
     return df
 
-
-x = []
-y = []
-z = []
-
-planets = []
-
-j = 0
-
-ThreeD = False
-
-while True:
-    if not os.path.exists(f'out{j:05d}.dat'):
-        break
-    # print(f'reading out{j:05d}.dat')
-    master_file = f'out{j:05d}.dat'
-
-    # if j >= 100:
-    #     break
-    if j % 10 == 0:
-        df = read_binary(master_file)
-        if j == 0:
-            for i in range(len(df)):
-                planets.append(Planet(df.loc[i]))
-        else:
-            for i in range(len(df)):
-                planets[i].add(df.loc[i])
-    j += 10
-if j == 0:
-    print("no files found, exiting...")
-    exit()
-else:
-    print("found {} files".format(j))
-
-j = len(planets[0].x)
-planets = planets[::1]
-planets[1].color = "brown"
-planets[2].color = "orange"
-planets[3].color = "blue"
-planets[4].color = "red"
-planets[5].color = "brown"
-planets[6].color = "orange"
-planets[7].color = "deepskyblue"
-planets[8].color = "blue"
-
-planets.reverse()
-
-print("found {} objects".format(len(planets)))
-
-# plt.plot(range(len(planets[0].dt)), planets[0].dt)
-# plt.savefig("dt.png")
-
-fig = plt.figure()
-
-fig.set_size_inches(5, 5, True)
-
-if ThreeD:
-    ax = p3.Axes3D(fig)
-else:
-    ax = fig.add_subplot(111)
-
-# ax.set_facecolor("black")
-
-colors = ["yellow" for i in range(len(planets))]
-
-if ThreeD:
-    lines = [ax.plot([], [], [], '-', linewidth=0.05, c=planet.color)[0]
-             for planet in planets]
-    pts = [ax.plot([], [], [], color=planet.color, marker=',', lw=0, markersize=0.5, linestyle="")[0]
-           for planet in planets]
-else:
-    lines = [ax.plot([], [], '-', linewidth=0.05, c=planet.color)[0]
-             for planet in planets]
-    pts = [ax.plot([], [], color=planet.color, marker='o', markersize=1, lw=0, linestyle="")[0]
-           for planet in planets]
-
-data = planets
-
-
-def init():
-    for line, pt in zip(lines, pts):
-        line.set_data([], [])
-        if ThreeD:
-            line.set_3d_properties([])
-        pt.set_data([], [])
-        if ThreeD:
-            pt.set_3d_properties([])
-    return lines + pts
-
-
-def animate(i):
-    print("processing step: ", i)
+if __name__ == "__main__":
     x = []
     y = []
     z = []
-    counter = 0
-    # print("T + {}".format(planets[0].t[i]))
-    # ax.set_title("T + {:.4f}".format(planets[0].t[i]), color="white", fontsize=4)
-    for line, pt, planet in zip(lines, pts, data):
 
-        x = planet.x[i]  # planet[0][i]
-        y = planet.y[i]  # planet[1][i]
-        z = planet.z[i]  # planet[2][i]
+    planets = []
 
-        xline = planet.x[:i]  # planet[0][:i]
-        yline = planet.y[:i]  # planet[1][:i]
-        zline = planet.z[:i]  # planet[2][:i]
+    j = 0
 
-        pt.set_data(x, y)
-        if ThreeD:
-            pt.set_3d_properties(z)
+    ThreeD = False
 
-        line.set_data(xline, yline)
-        if ThreeD:
-            line.set_3d_properties(zline)
-        counter += 1
+    while True:
+        if not os.path.exists(f'out{j:05d}.dat'):
+            break
+        # print(f'reading out{j:05d}.dat')
+        master_file = f'out{j:05d}.dat'
+
+        # if j >= 100:
+        #     break
+        if j % 10 == 0:
+            df = read_binary(master_file)
+            if j == 0:
+                for i in range(len(df)):
+                    planets.append(Planet(df.loc[i]))
+            else:
+                for i in range(len(df)):
+                    planets[i].add(df.loc[i])
+        j += 10
+    if j == 0:
+        print("no files found, exiting...")
+        exit()
+    else:
+        print("found {} files".format(j))
+
+    j = len(planets[0].x)
+    planets = planets[::1]
+    planets[1].color = "brown"
+    planets[2].color = "orange"
+    planets[3].color = "blue"
+    planets[4].color = "red"
+    planets[5].color = "brown"
+    planets[6].color = "orange"
+    planets[7].color = "deepskyblue"
+    planets[8].color = "blue"
+
+    planets.reverse()
+
+    print("found {} objects".format(len(planets)))
+
+    # plt.plot(range(len(planets[0].dt)), planets[0].dt)
+    # plt.savefig("dt.png")
+
+    fig = plt.figure()
+
+    fig.set_size_inches(5, 5, True)
+
+    if ThreeD:
+        ax = p3.Axes3D(fig)
+    else:
+        ax = fig.add_subplot(111)
+
+    # ax.set_facecolor("black")
+
+    colors = ["yellow" for i in range(len(planets))]
+
+    if ThreeD:
+        lines = [ax.plot([], [], [], '-', linewidth=0.05, c=planet.color)[0]
+                 for planet in planets]
+        pts = [ax.plot([], [], [], color=planet.color, marker=',', lw=0, markersize=0.5, linestyle="")[0]
+               for planet in planets]
+    else:
+        lines = [ax.plot([], [], '-', linewidth=0.05, c=planet.color)[0]
+                 for planet in planets]
+        pts = [ax.plot([], [], color=planet.color, marker='o', markersize=1, lw=0, linestyle="")[0]
+               for planet in planets]
+
+    data = planets
 
 
-AU = 1.5e11
+    def init():
+        for line, pt in zip(lines, pts):
+            line.set_data([], [])
+            if ThreeD:
+                line.set_3d_properties([])
+            pt.set_data([], [])
+            if ThreeD:
+                pt.set_3d_properties([])
+        return lines + pts
 
-size = 1.6 * AU
 
-com = np.zeros(3)
-m = 0
+    def animate(i):
+        print("processing step: ", i)
+        x = []
+        y = []
+        z = []
+        counter = 0
+        # print("T + {}".format(planets[0].t[i]))
+        # ax.set_title("T + {:.4f}".format(planets[0].t[i]), color="white", fontsize=4)
+        for line, pt, planet in zip(lines, pts, data):
 
-# for planet in planets:
-#     com += np.mean(np.array([planet.x,planet.y,planet.z]),axis=1)*planet.m
-#     m += planet.m
+            x = planet.x[i]  # planet[0][i]
+            y = planet.y[i]  # planet[1][i]
+            z = planet.z[i]  # planet[2][i]
 
-# com /= m
+            xline = planet.x[:i]  # planet[0][:i]
+            yline = planet.y[:i]  # planet[1][:i]
+            zline = planet.z[:i]  # planet[2][:i]
 
-print(com)
+            pt.set_data(x, y)
+            if ThreeD:
+                pt.set_3d_properties(z)
 
-# for planet in planets:
-#     planet.x += com[0]
-#     planet.y += com[1]
-#     planet.z += com[2]
+            line.set_data(xline, yline)
+            if ThreeD:
+                line.set_3d_properties(zline)
+            counter += 1
 
-R = [np.linalg.norm([planet.x, planet.y, planet.z]) for planet in planets]
-size = np.max(R) / 4
 
-# size = 1
+    AU = 1.5e11
 
-plt.axis('off')
-# Setting the axes properties
-ax.set_xlim(-2, 2)
-ax.set_ylim(-2, 2)
+    size = 1.6 * AU
 
-# ax.set_xlim(-1.5, 1)
-# ax.set_ylim(8, 10)
+    com = np.zeros(3)
+    m = 0
 
-x = np.max([abs(p.x[0]) for p in planets])
-y = np.max([abs(p.y[0]) for p in planets])
-ax.set_xlim(-1 * x, 1 * x)
-ax.set_ylim(-1 * y, 1 * y)
+    # for planet in planets:
+    #     com += np.mean(np.array([planet.x,planet.y,planet.z]),axis=1)*planet.m
+    #     m += planet.m
 
-if ThreeD:
-    ax.set_zlim(-size, size)
-    ax.set_zlim(-10, 10)
+    # com /= m
 
-gif = False
-mp4 = False  # True
-show = True
+    print(com)
 
-ax.set_aspect('equal')
+    # for planet in planets:
+    #     planet.x += com[0]
+    #     planet.y += com[1]
+    #     planet.z += com[2]
 
-# ax.set_facecolor("black")
+    R = [np.linalg.norm([planet.x, planet.y, planet.z]) for planet in planets]
+    size = np.max(R) / 4
 
-ani = animation.FuncAnimation(fig, animate, init_func=init,
-                              frames=j, interval=1, blit=False)
+    # size = 1
 
-# save animation
-if gif:
-    print("saving gif...")
-    ani.save(f'{file_name}.gif', savefig_kwargs={'facecolor': 'black'}, writer='imagemagick', dpi=100)
-if mp4:
-    # needs ffmpeg to be installed
-    mywriter = animation.FFMpegWriter(fps=24)
-    print("saving mp4...")
-    ani.save(f'{file_name}.mp4', savefig_kwargs={'facecolor': 'black'}, writer=mywriter, dpi=600)
-if show:
-    print("showing")
-    plt.show()
+    plt.axis('off')
+    # Setting the axes properties
+    ax.set_xlim(-2, 2)
+    ax.set_ylim(-2, 2)
+
+    # ax.set_xlim(-1.5, 1)
+    # ax.set_ylim(8, 10)
+
+    x = np.max([abs(p.x[0]) for p in planets])
+    y = np.max([abs(p.y[0]) for p in planets])
+    ax.set_xlim(-1 * x, 1 * x)
+    ax.set_ylim(-1 * y, 1 * y)
+
+    if ThreeD:
+        ax.set_zlim(-size, size)
+        ax.set_zlim(-10, 10)
+
+    gif = False
+    mp4 = False  # True
+    show = True
+
+    ax.set_aspect('equal')
+
+    # ax.set_facecolor("black")
+
+    ani = animation.FuncAnimation(fig, animate, init_func=init,
+                                  frames=j, interval=1, blit=False)
+
+    # save animation
+    if gif:
+        print("saving gif...")
+        ani.save(f'{file_name}.gif', savefig_kwargs={'facecolor': 'black'}, writer='imagemagick', dpi=100)
+    if mp4:
+        # needs ffmpeg to be installed
+        mywriter = animation.FFMpegWriter(fps=24)
+        print("saving mp4...")
+        ani.save(f'{file_name}.mp4', savefig_kwargs={'facecolor': 'black'}, writer=mywriter, dpi=600)
+    if show:
+        print("showing")
+        plt.show()
